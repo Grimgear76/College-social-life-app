8c067e70c637fef57ea2d29edc819a745bc68ca3
CookieCutter07 <147435622+CookieCutter07@users.noreply.github.com>
2025-10-05 12:57:49 +0000

    added the first html page, set up the get request to the page, added css file

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const express = require("express")
 //variable app calls the express function, which makes an instance of an Express application
 const app = express()
 
+//set ejs framework to allow dynamic html pages
+app.set("view engine", "ejs")
+
 //get request from root (/) to make a callback function whenever someone uses a get request
 app.get("/", (req, res) => {
     //the server sends "working" after client sends get request
  res.send("working")
+
+    //the server renders the hompage view after client sends get request
+ res.render("homepage")
 })
 
-//app to listen at port 3000
-app.listen(3000)
+//set port Number and includes environment through 'process.env' to "PORT" variable
+const PORT = process.env.PORT || 3000 
+
+//app to listen at "PORT"
+app.listen(PORT)
