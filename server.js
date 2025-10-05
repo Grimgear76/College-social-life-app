e969a2b731f6b8ed01c8900ebdfbd69e94c993f7
CookieCutter07 <147435622+CookieCutter07@users.noreply.github.com>
2025-10-05 13:41:33 +0000

    cleaned up project

--- server.js
+++ server.js
@@ -3,27 +3,22 @@ const express = require("express")
 
 //variable app calls the express function, which makes an instance of an Express application
 const app = express()
+//set port Number and includes environment through 'process.env' to "PORT" variable
+const PORT = process.env.PORT || 3000;
 
 //set ejs framework to allow dynamic html pages
 app.set("view engine", "ejs")
-app.use(express.urlencoded({extended: false})) 
-
 //makes sure to include the public folder to the server
 app.use(express.static("public"))
 
 
-
 //get request from root (/) to make a callback function whenever someone uses a get request
 app.get("/", (req, res) => {
-    //the server sends "working" after client sends get request
-// res.send("working")
-
     //the server renders the hompage view after client sends get request
  res.render("homepage")
 })
 
-//set port Number and includes environment through 'process.env' to "PORT" variable
-const PORT = process.env.PORT || 3000;
+
 
 //app to listen at "PORT"
 app.listen(PORT, () => {
