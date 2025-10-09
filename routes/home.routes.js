137e3c481efb7edf39b360c90a4dfe5f56eb8846
CookieCutter07 <147435622+CookieCutter07@users.noreply.github.com>
2025-10-09 01:42:25 +0000

    changed backend template

--- routes/home.routes.js
+++
@@ -1,9 +0,0 @@
-// routes/home.routes.js
-const express = require("express");
-const router = express.Router();
-const homeController = require("../controllers/home.controller");
-
-// When user visits "/", render homepage
-router.get("/", homeController.getHomePage);
-
-module.exports = router;
--- routes/posts.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const postsController = require("../controllers/posts.controller");
-
-router.get("/", postsController.getPosts);
-router.post("/", postsController.addPost);
-
-module.exports = router;
