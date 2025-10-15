97b8cc9ab7d5a27b8c190c83e672490bf4981b42
CookieCutter07 <147435622+CookieCutter07@users.noreply.github.com>
2025-10-15 02:12:43 +0000

    backend

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,10 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js"
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from './middleware/auth.js';
 
 
 /* Configurations and Middleware */
@@ -46,6 +49,7 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 /* ROUTES WITH FILES END */
 
 
@@ -53,6 +57,7 @@ app.post("/auth/register", upload.single("picture"), register);
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.user("/posts", postRoutes);
 /* ROUTES END */
 
 /* MONGOOSE SETUP */
