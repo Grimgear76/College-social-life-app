5175cbb368726552b39fb9ec234fd53714ac0020
CookieCutter07 <147435622+CookieCutter07@users.noreply.github.com>
2025-10-22 21:36:20 +0000

    client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,40 @@
 //app.jsx has the job of keeping the app running smoothly after main.jsx sets up the webapp
 
-import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
-import HomePage from 'scenes/homePage';
-import ProfilePage from 'scenes/profilePage';
+import ProfilePage from "scenes/profilePage";
 import { useMemo } from "react";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
-import { themeSettings } from './theme';
-
+import { themeSettings } from "./theme";
 
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
-    <div className = "app">
-        <BrowserRouter>
+    <div className="app">
+      <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-            <Route path="/home" element={<HomePage />} />  {/* This is our home page */}
-            <Route path="/" element={<LoginPage />} /> {/* This is our login page */}
-            <Route path="/profile/:userId" element={<ProfilePage />} />  {/* This is our profile page */}
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
           </Routes>
-          </ThemeProvider>
-        </BrowserRouter>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,12 @@
 //Main.jsx has the job of sets up the webapp for the app.jsx file to be able to run it smoothly.
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-//import './index.css';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import authReducer from "./state";
 import { configureStore } from "@reduxjs/toolkit";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import {
   persistStore,
   persistReducer,
@@ -15,54 +15,30 @@ import {
   PAUSE,
   PERSIST,
   PURGE,
-  REGISTER
+  REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
 
-//saves state on local computer (doesn't drop state unless cache is cleared)
-const persistConfig = {key: "root", storage, version: 1 };
+const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
-  reducer: persistedReducer, 
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: [FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER]
-      }
-    })
-})
-
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistStore(store)}>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
         <App />
-        </PersistGate>
-      </Provider>
-    </React.StrictMode>,
-);                  
-
-
-
-
-
-
-
-
-
-
-
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <Provider store={store}>
-//       <PersistGate loading={null} persistor={persistStore(store)}>
-//       <App />
-//       </PersistGate>
-//     </Provider>
-    
-//   </StrictMode>,
-// )
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
