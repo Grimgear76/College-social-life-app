5196b6915f06361242b4d7cf55c3f0ad18e84ed7
CookieCutter07 <dustinboston22@gmail.com>
2025-11-06 11:10:53 -0600

    first commit vs

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -64,8 +64,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "https://redesigned-yodel-wr76qjrwwqwq25pwx-3001.app.github.dev/auth/register",
-      //"http://localhost:3001/auth/register",
+      "http://localhost:3001/auth/register",
       {
         method: "POST",
         body: formData,
@@ -80,7 +79,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("https://redesigned-yodel-wr76qjrwwqwq25pwx-3001.app.github.dev/auth/login", {
+    const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values), // this is the credentials
